5ec903c8cbcdb848caa2d6a4399e04e5694a9306
Pawankashap <pawan.3940@gmail.com>
2023-06-28 18:37:13 -0700

    Updates favorite value state and db.json

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -3,43 +3,45 @@ import { Container } from 'bootstrap-4-react';
 import Moviescard from "../Moviescard/Moviescard";
 import "../Movies/Movies.css"
 
-function Favorites({movies,setMovie,handleDelete}) {
-    const [filteredMovies, setFilteredMovies] = useState([])
-    useEffect(() => {
+function Favorites({movies,setMovie,filteredMovies,setFilteredMovies,handleDelete}) {
+  
+  // const [filteredMovies, setFilteredMovies] = useState([])
+  
+  useEffect(() => {
       setFilteredMovies(movies.filter(movie => movie.favorite === true));
   }, [])
 
   const handleFavorite=(id,isfavorite)=>{
-    const updatedRecords = filteredMovies.map(record => {
-      if (record.id === parseInt(id,10)) {
-          return { ...record, 'favorite': isfavorite };
-      }
-      return record;
-      });
-    const updatedRecordsmovies = movies.map(record => {
-        if (record.id === parseInt(id,10)) {
-            return { ...record, 'favorite': isfavorite };
-        }
-        return record;
-    });
-      
-    fetch(`http://localhost:3001/movies/${id}`, {
-        method: 'PATCH',
-        headers: {
-           Accept: "application/json",
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            favorite:isfavorite
+        const updatedRecords = filteredMovies.map(record => {
+            if (record.id === parseInt(id,10)) {
+                return { ...record, 'favorite': isfavorite };
+            }
+            return record;
+            });
+        const updatedRecordsmovies = movies.map(record => {
+            if (record.id === parseInt(id,10)) {
+                return { ...record, 'favorite': isfavorite };
+            }
+            return record;
+        });
+          
+        fetch(`http://localhost:3001/movies/${id}`, {
+            method: 'PATCH',
+            headers: {
+              Accept: "application/json",
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                favorite:isfavorite
+            })
         })
-      })
         .then(response => response.json())
         .then(data => {
-            setMovie(updatedRecordsmovies)
+            setMovie(movies=>updatedRecordsmovies)
             setFilteredMovies(filteredMovies=>updatedRecords.filter(movie => movie.favorite === true))
         })
         .catch(error => console.log(error));
-}
+  }
 
     const movieDetails = filteredMovies.map((movie) => (
       <Moviescard
